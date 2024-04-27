1d338e72ef80636ec1bae5c5e1dda0fc7b7b9eec
YoselinMA <162406301+YoselinMA@users.noreply.github.com>
2024-04-26 22:12:58 -0600

    Create Whileejercicioq.java

---
+++ Whileejercicioq.java
@@ -0,0 +1,14 @@
+public class Whileejercicio1 {
+    
+    public static void main(String[] args) {
+        int numero = 1;
+
+        while (numero<=100){
+            int v= numero * numero;
+            int c= numero* numero * numero;
+            System.out.println(v+"\n"+c);
+            numero ++;
+
+        }
+    }
+}
